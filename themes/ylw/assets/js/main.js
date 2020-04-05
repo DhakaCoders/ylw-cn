6013186dd72174569af485ce90b7a784fe46e4c4
Rannojit <cserannojitsarkar22@gmail.com>
2020-04-05 12:14:08 +0600

    home 3 section done

--- themes/ylw/assets/js/main.js
+++ themes/ylw/assets/js/main.js
@@ -155,6 +155,46 @@ $('.faq-accordion-tab-row').removeClass('remove-border');
 
 
 /*End of Rannojit*/
+if( $('.hmNewAarrivalsSlider').length ){
+    $('.hmNewAarrivalsSlider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: $('.hm-new-arrivals-sec .fl-prev'),
+      nextArrow: $('.hm-new-arrivals-sec .fl-next'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
 
 
 })(jQuery);
