979f4eb6573fcaa96d21c18459fb583bee20977a
Rannojit <cserannojitsarkar22@gmail.com>
2020-04-14 21:14:48 +0600

    ..

--- themes/ylw/assets/js/main.js
+++ themes/ylw/assets/js/main.js
@@ -239,24 +239,19 @@ if( $('.hmNewAarrivalsSlider').length ){
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
+            slidesToShow: 3
           }
         },
         {
-          breakpoint: 600,
+          breakpoint: 640,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 2
           }
         },
         {
-          breakpoint: 480,
+          breakpoint: 576,
           settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 1
           }
         }
         // You can unslick at a given breakpoint now by adding:
