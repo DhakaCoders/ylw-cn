9c6fd6714846c2f88e82229593778c25dfc2c9c0
Rannojit <cserannojitsarkar22@gmail.com>
2020-04-30 18:16:27 +0600

    home page container updated

--- themes/ylw/footer.php
+++ themes/ylw/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer-wrp">
-  <div class="container-fluid">
+  <div class="container-xlg">
     <div class="row">
       <div class="col-12">
         <div class="ftr-col-main clearfix">
